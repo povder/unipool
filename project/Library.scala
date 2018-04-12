12ff6694b289354fbd4a50a8ac9cde4e7a1b86b9
Krzysztof Pado <povder@gmail.com>
2018-04-11 18:41:08 -0700

    Update dependencies

--- project/Library.scala
+++ project/Library.scala
@@ -3,9 +3,11 @@ import sbt._
 object Library {
   val stm = "org.scala-stm" %% "scala-stm" % "0.8"
   val immutables = "org.immutables" % "value" % "2.5.6"
-  val scalaLogging = "com.typesafe.scala-logging" %% "scala-logging" % "3.7.2"
+  val scalaLogging = "com.typesafe.scala-logging" %% "scala-logging" % "3.8.0"
   val java8Compat = "org.scala-lang.modules" %% "scala-java8-compat" % "0.8.0"
 
-  val scalatest = "org.scalatest" %% "scalatest" % "3.0.4"
+  val scalatest = "org.scalatest" %% "scalatest" % "3.0.5"
   val scalamock = "org.scalamock" %% "scalamock-scalatest-support" % "3.6.0"
+
+  val sourcecode = "com.lihaoyi" %% "sourcecode" % "0.1.3"
 }
